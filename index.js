b738c380a711c84e6f28fcda127425f9df943f26
KaanOzen-mF <kaanozn98@gmail.com>
2023-07-17 22:52:12 +0300

    Functional check btn added also all button works very fine

--- index.js
+++ index.js
@@ -1,183 +1,89 @@
-const catsData = [
-    {
-        emotionTags: ["moody"],
-        isGif: false,
-        image: "angry.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["moody", "insomniac"],
-        isGif: false,
-        image: "angry2.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["moody"],
-        isGif: false,
-        image: "angry3.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["confused", "sad"],
-        isGif: false,
-        image: "confused.jpeg",
-        alt: "A cat looking confused",
-    },
-    {
-        emotionTags: ["dominant", "moody"],
-        isGif: false,
-        image: "dominant.jpeg",
-        alt: "A cat looking dominant",
-    },
-    {
-        emotionTags: ["happy", "relaxed"],
-        isGif: false,
-        image: "happy.jpeg",
-        alt: "A cat looking happy",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: false,
-        image: "hungry.jpeg",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: false,
-        image: "hungry1.jpeg",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["insomniac"],
-        isGif: false,
-        image: "insomnia.jpeg",
-        alt: "A cat looking insomniac",
-    },
-    {
-        emotionTags: ["insomniac"],
-        isGif: false,
-        image: "insomnia1.jpeg",
-        alt: "A cat looking insomniac",
-    },
-    {
-        emotionTags: ["relaxed"],
-        isGif: false,
-        image: "lazy.jpeg",
-        alt: "A cat looking lazy",
-    },
-    {
-        emotionTags: ["scared"],
-        isGif: false,
-        image: "nervous.jpeg",
-        alt: "A cat looking nervous",
-    },
-    {
-        emotionTags: ["sad"],
-        isGif: false,
-        image: "sad.jpeg",
-        alt: "A cat looking sad",
-    },
-    {
-        emotionTags: ["sad", "moody"],
-        isGif: false,
-        image: "sad1.jpeg",
-        alt: "A cat looking sad",
-    },
-    {
-        emotionTags: ["moody"],
-        isGif: true,
-        image: "angry.gif",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["moody"],
-        isGif: true,
-        image: "angry2.gif",
-        alt: "A cat looking angry",
-    },
-    {
-        emotionTags: ["confused"],
-        isGif: true,
-        image: "confused2.gif",
-        alt: "A cat looking confused",
-    },
-    {
-        emotionTags: ["dominant"],
-        isGif: true,
-        image: "dominant.gif",
-        alt: "A cat looking dominant",
-    },
-    {
-        emotionTags: ["happy"],
-        isGif: true,
-        image: "happy.gif",
-        alt: "A cat looking happy",
-    },
-    {
-        emotionTags: ["hungry", "sad", "confused"],
-        isGif: true,
-        image: "confused.gif",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: true,
-        image: "hungry.gif",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: true,
-        image: "hungry2.gif",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["insomniac", "scared"],
-        isGif: true,
-        image: "insomnia2.gif",
-        alt: "A cat looking insomniac",
-    },
-    {
-        emotionTags: ["relaxed"],
-        isGif: true,
-        image: "lazy.gif",
-        alt: "A cat looking relaxed",
-    },
-    {
-        emotionTags: ["relaxed"],
-        isGif: true,
-        image: "relaxed2.gif",
-        alt: "A cat looking relaxed",
-    },
-    {
-        emotionTags: ["scared", "sad"],
-        isGif: true,
-        image: "nervous.gif",
-        alt: "A cat looking nervous",
-    },
-    {
-        emotionTags: ["scared"],
-        isGif: true,
-        image: "nervous2.gif",
-        alt: "A cat looking scared",
-    },
-    {
-        emotionTags: ["sad"],
-        isGif: true,
-        image: "sad.gif",
-        alt: "A cat looking sad",
-    },
-]
+import { catsData } from "./data" // cats data
 
-function getEmotionsArray(cats){
-    const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
-        }
+const emotionRadios = document.getElementById("emotion-radios")
+const getImgBtn = document.getElementById("get-image-btn")
+const gifOnlyBtn = document.getElementById("gifs-only-option")
+
+emotionRadios.addEventListener("change",highlightedCheckedOption)//listener for changing radios button
+getImgBtn.addEventListener("click",renderCat)//listener for submit button
+
+function getMatchingCatsArray(){//with this function we control checked btn and according to this function sumbit button return to user what he/she choosed option
+   //returns an array of cat objects that matches the user's criteria
+    if(document.querySelector("input[type='radio']:checked")){
+        const selectedEmotion = document.querySelector("input[type='radio']:checked").value
+        const isGif = gifOnlyBtn.checked //gif only option check
+        const matchingCatsArray = catsData.filter((cat) => {
+            if (isGif) {
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            } else {
+                return cat.emotionTags.includes(selectedEmotion)
+            }
+        })
+        return matchingCatsArray
+
     }
-console.log(emotionsArray)
 }
 
-// getEmotionsArray(catsData)
+function getSingleCatObject(){
+// will return a single cat object selected from the array provided by getMatchingCatsArray
+    
+    const catsArray = getMatchingCatsArray()
+    
+    if(catsArray.length === 1){
 
+        console.log(catsArray)
+    }
+}
+
+
+function renderCat(){
+//will use the cat object provided by getSingleCatObject to create HTML string which it will render it to the dom
+    getSingleCatObject()
+}
+
+
+function highlightedCheckedOption(e){ //remove all higlighted options with that function
+    const radios = document.getElementsByClassName("radio")
+    for(let radio of radios){//rerender radio list for remove highlighted classes
+        radio.classList.remove("highlight")
+    }
+    document.getElementById(e.target.id).parentElement.classList.add("highlight")//add highlight class
+
+}
+
+
+function getEmotionsArray(cats){ // we returned cats emotion array using that function
+    const emotionsArray = []
+    for (let cat of cats){//we loop over all cats data for searching cats emotion
+        for (let emotion of cat.emotionTags){
+            if(!emotionsArray.includes(emotion)){//we delete duplicate emotion data
+                    emotionsArray.push(emotion)
+                }
+        }
+    }
+    return emotionsArray
+}
+
+function renderEmotionsRadios(cats){ // according to our cats emotion array we create radio buttons using that function
+    let emotionRadioItems = ""
+    const emotions = getEmotionsArray(catsData)
+    for (let emotion of emotions){
+        emotionRadioItems += `
+        <div class="radio">
+            <label for=${emotion}>${emotion}</label>
+                                <input
+                                    type="radio"                    
+                                    id=${emotion}
+                                    value=${emotion}
+                                    name="emotions"
+                                    
+                                    >
+        </div>
+        `
+    }
+    emotionRadios.innerHTML = emotionRadioItems
+}
+
+
+
+renderEmotionsRadios(catsData)
